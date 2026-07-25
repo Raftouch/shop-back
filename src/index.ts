8330ca4257b44bb3308f93ee7b47dbd042c53dc3
raftouch <rafanelkin@gmail.com>
2026-07-25 19:43:09 +0200

    feat(api): add test route for prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
+import { prisma } from "../lib/prisma";
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+app.get("/", async (req, res) => {
+  try {
+    // const user = await prisma.user.create({
+    //   data: {
+    //     email: "bob@marley.com",
+    //     password: "test",
+    //   },
+    // });
+
+    const users = await prisma.user.findMany();
+
+    // console.log("All users:", JSON.stringify(users));
+
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
