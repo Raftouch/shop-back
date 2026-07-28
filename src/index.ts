aa09b3aec454f90f83abb8092b7d2166460b857a
raftouch <rafanelkin@gmail.com>
2026-07-28 16:02:16 +0200

    feat(user): add endpoint to fetch users

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import { prisma } from "../lib/prisma";
 import cors from "cors";
+import userRoute from "../routes/user";
 
 dotenv.config();
 
@@ -11,11 +11,11 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
+app.use("/user", userRoute);
+
 app.get("/", async (req, res) => {
   try {
-    const users = await prisma.user.findMany();
-
-    res.json(users);
+    res.json({ message: "Welcome to the Shop API" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
