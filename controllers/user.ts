d28f2d7920d01c86352eaf0c43ae978eb68fab95
raftouch <rafanelkin@gmail.com>
2026-07-29 14:19:21 +0200

    refactor(user): add Express request and response types

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,11 +1,13 @@
-export const authCheck = async (req, res) => {
+import { Request, Response } from "express";
+
+export const authCheck = async (req: Request, res: Response) => {
   res.json({ message: "Auth route" });
 };
 
-export const login = async (req, res) => {
+export const login = async (req: Request, res: Response) => {
   res.json({ message: "Login route" });
 };
 
-export const register = async (req, res) => {
+export const register = async (req: Request, res: Response) => {
   res.json({ message: "Register route" });
 };
