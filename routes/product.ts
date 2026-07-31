259008fdc595f81a65bf76c7989a95dcd9f4dc03
raftouch <rafanelkin@gmail.com>
2026-07-31 15:20:43 +0200

    refactor(product): move route handlers to controller

--- routes/product.ts
+++ routes/product.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import { create, getAll, getOne } from "../controllers/product";
 
 const router = express.Router();
 
-router.post("/");
-router.get("/");
-router.get("/:id");
+router.post("/", create);
+router.get("/", getAll);
+router.get("/:id", getOne);
 
 export default router;
