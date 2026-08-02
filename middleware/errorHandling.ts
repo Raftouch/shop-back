98c80de484ee2620b6b7ee35390fc1e4f2c3d814
raftouch <rafanelkin@gmail.com>
2026-08-02 18:36:51 +0200

    feat(error): add error handling middleware

---
+++ middleware/errorHandling.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { ApiError } from "../error/apiError";
+
+export const errorHandler = (
+  err: unknown,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (err instanceof ApiError) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    message: "Internal server error",
+  });
+};
