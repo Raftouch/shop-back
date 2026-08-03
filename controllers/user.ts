c02fe892711436775c9a297b6871fd223b620072
raftouch <rafanelkin@gmail.com>
2026-08-03 19:27:40 +0200

    feat(user): integrate error handling

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,13 +1,24 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { ApiError } from "../error/apiError";
 
-export const authCheck = async (req: Request, res: Response) => {
-  res.json({ message: "Auth route" });
+export const register = async (req: Request, res: Response) => {
+  res.json({ message: "Register route" });
 };
 
 export const login = async (req: Request, res: Response) => {
   res.json({ message: "Login route" });
 };
 
-export const register = async (req: Request, res: Response) => {
-  res.json({ message: "Register route" });
+export const authCheck = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { id } = req.query;
+
+  if (!id) {
+    return next(ApiError.badRequest());
+  }
+
+  res.json({ message: "Auth route", id });
 };
