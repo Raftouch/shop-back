9a846f0956df3e4384882c301db1b5721dd99e17
raftouch <rafanelkin@gmail.com>
2026-08-09 15:38:09 +0200

    feat(brand): add create brand logic

--- controllers/brand.ts
+++ controllers/brand.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from "express";
+import { prisma } from "../lib/prisma";
 
 export const create = async (req: Request, res: Response) => {
-  res.json({ message: "Create brand route" });
+  try {
+    const { name } = req.body;
+    const brand = await prisma.brand.create({ data: { name: name } });
+
+    res.status(201).json(brand);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to create brand", error });
+  }
 };
 
 export const getAll = async (req: Request, res: Response) => {
