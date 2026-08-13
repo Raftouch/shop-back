2962d143d85b7fefa9c00e20fb0bde24c707b777
raftouch <rafanelkin@gmail.com>
2026-08-13 18:16:30 +0200

    feat(product): add logic to create a new product

--- controllers/product.ts
+++ controllers/product.ts
@@ -1,7 +1,26 @@
 import { Request, Response } from "express";
+import { v4 as uuid } from "uuid";
+import path from "path";
+import { prisma } from "../lib/prisma";
 
 export const create = async (req: Request, res: Response) => {
-  res.json({ message: "Create product route" });
+  const { name, price, typeId, brandId, info } = req.body;
+  const { img } = req.files;
+
+  let fileName = uuid() + ".jpg";
+  img.mv(path.resolve(__dirname, "..", "static", fileName));
+
+  const product = await prisma.product.create({
+    data: {
+      name,
+      price,
+      brandId,
+      typeId,
+      img: fileName,
+    },
+  });
+
+  return res.json(product);
 };
 
 export const getAll = async (req: Request, res: Response) => {
